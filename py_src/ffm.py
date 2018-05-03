fc9f297f60fd12a9f5aa381c40e3e3d96744430b
Yunang Chen <cya.tempsperdu@gmail.com>
2018-05-03 01:45:42 -0500

    add prediction codes

--- py_src/ffm.py
+++ py_src/ffm.py
@@ -25,11 +25,14 @@ class FFM:
             self._fit_adagrad((X_val[minibatch_indices], X_feat[minibatch_indices]),
                               y[minibatch_indices], feature2field)
 
+            if iteration_idx % 50 == 0:
+                print('Iteration: ' + str(iteration_idx))
+
     def _fit_adagrad(self, X, y, feature2field: dict):
         X_val, X_feat = X  # type: (np.ndarray, np.ndarray)
         X_field = np.vectorize(feature2field.__getitem__)(X_feat)
 
-        n_samples, n_fields = X_val.shape
+        _, n_fields = X_val.shape
 
         coef_idx = (np.repeat(X_feat, n_fields, axis=-1), np.tile(X_field, n_fields))
         coef = self.coef_[coef_idx[0], coef_idx[1]]
@@ -50,4 +53,20 @@ class FFM:
         np.add.at(self.g_sum_, coef_idx, gradient ** 2)
         np.add.at(self.coef_, coef_idx, - self.learning_rate / np.sqrt(self.g_sum_[coef_idx]) * gradient)
 
-    
+    def predict(self, X, feature2field: dict):
+        X_val, X_feat = X
+        X_field = np.vectorize(feature2field.__getitem__)(X_feat)
+
+        n_samples, n_fields = X_val.shape
+
+        coef_idx = (np.repeat(X_feat, n_fields, axis=-1), np.tile(X_field, n_fields))
+        coef = self.coef_[coef_idx[0], coef_idx[1]]
+
+        X_val_repeated = np.repeat(X_val, n_fields, axis=-1)[..., None]
+        coef_val = (coef * X_val_repeated).reshape(n_samples, n_fields, n_fields, -1)
+
+        triu = np.triu_indices(n_fields, k=1)
+        kernel_ffm = np.sum(coef_val.transpose((0, 2, 1, 3))[:, triu[0], triu[1], :]
+                            * coef_val[:, triu[0], triu[1], :], axis=(1, 2))
+
+        return np.sign(kernel_ffm)
